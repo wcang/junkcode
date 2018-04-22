4e321d553cc1d0a4472369469c166afa35ef3ff2
Ang Way Chuang <wcang79@gmail.com>
2018-04-22 13:59:54 +0800

    Try out example to call java class from kotlin

---
+++ kotlindemo/src/main/java/com/wcang/kotlindemo/JavaPerson.java
@@ -0,0 +1,19 @@
+package com.wcang.kotlindemo;
+
+public class JavaPerson {
+    private String name;
+    private int age;
+
+    public JavaPerson(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
--- kotlindemo/src/main/kotlin/com.wcang.kotlindemo.main.kt
+++ kotlindemo/src/main/kotlin/com.wcang.kotlindemo.main.kt
@@ -1,4 +1,41 @@
+import com.wcang.kotlindemo.JavaPerson;
+
+fun simpleFunction(aString: String) = println(aString)
 
 fun main(args: Array<String>) {
-    println("Hello World")
+    val name = if (args.size > 0) args[0] else "Kotlin"
+    println("Hello $name")
+
+    var aString: String
+    aString = "something"
+
+    when (aString) {
+        "something" -> println("it's something")
+        else -> {
+            println(aString)
+        }
+    }
+
+    val aNum = 2
+
+    when (aNum) {
+        in 1..2 -> println("1 to 2")
+        in 2..5 -> println("2 to 5")
+    }
+
+    for (item in 1.rangeTo(10).step(2))
+        println("$item")
+
+    simpleFunction("How do you do?")
+    /* kotlin checks for null value during compile time
+    var uninitializedString: String
+    simpleFunction(uninitializedString)
+    */
+    val initLater: String
+    initLater = "something"
+    simpleFunction(initLater)
+
+    val person = JavaPerson("Ang", 38)
+    println("${person.name}'s age is ${person.age} ")
+
 }
